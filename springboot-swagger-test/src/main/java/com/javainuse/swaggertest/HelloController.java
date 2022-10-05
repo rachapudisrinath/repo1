b0c50b009d46f3c0211417e7dd4d0c081003b84e
HP <rachapudisrinath@gmail.com>
2022-10-05 09:56:38 +0530

    feature commit

--- springboot-swagger-test/src/main/java/com/javainuse/swaggertest/HelloController.java
+++ springboot-swagger-test/src/main/java/com/javainuse/swaggertest/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/api/test")
+	@RequestMapping(method = RequestMethod.GET, value = "/api/test1")
 	public String sayHello() {
-		return "Swagger Hello World";
+		return "Swagger Hello World"; 
 	}
 }
