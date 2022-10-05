d948c6d9d07daf8445131e3a758534c0fb0b7ef5
rachapudisrinath <108725282+rachapudisrinath@users.noreply.github.com>
2022-10-05 10:47:42 +0530

    Update SwaggerConfig.java

--- springboot-swagger-test/src/main/java/com/javainuse/swaggertest/SwaggerConfig.java
+++ springboot-swagger-test/src/main/java/com/javainuse/swaggertest/SwaggerConfig.java
@@ -22,17 +22,18 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
 				.apiInfo(apiInfo()).select().paths(postPaths()).build();
 	}
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("JavaInUse API")
+				.description("JavaInUse API reference for developers")
+				.termsOfServiceUrl("http://javainuse.com")
+				.contact("javainuse@gmail.com").license("JavaInUse ")
+				.licenseUrl("javainuse@gmail.com").version("1.0").build();
+	}
+
 
 	private Predicate<String> postPaths() {
 		return or(regex("/api/posts.*"), regex("/api/*.*"));
 	}
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("JavaInUse API")
-				.description("JavaInUse API reference for developers")
-				.termsOfServiceUrl("http://javainuse.com")
-				.contact("javainuse@gmail.com").license("JavaInUse License")
-				.licenseUrl("javainuse@gmail.com").version("1.0").build();
-	}
-
+	
 }
